a1ca4b22748cc9d0cb0d01aaecf7af150f4c030e
newyoung124 <newyoung124@naver.com>
2023-04-14 21:05:05 +0900

    default settings

--- src/main/java/org/imlog/Main.java
+++
@@ -1,7 +0,0 @@
-package org.imlog;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/test/java/com/imlog/HelloControllerTest.java
@@ -0,0 +1,40 @@
+package com.imlog;
+
+import com.imlog.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello_will_be_returned() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloResponeDto_will_be_returned() throws Exception {
+
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto").param("name", name).param("amount",String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(result->{jsonPath("$.name", is(name));})
+                .andExpect(result->{jsonPath("$.amount", is(amount));});
+    }
+}

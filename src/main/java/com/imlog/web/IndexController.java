9df3fca775371ff8b747a45d8bf9720fcb70af0c
newyoung124 <newyoung124@naver.com>
2023-04-21 00:13:33 +0900

    mustache 추가, index.mustache 파일 로드 테스트, 브라우저에서 로드 확인

---
+++ src/main/java/com/imlog/web/IndexController.java
@@ -0,0 +1,11 @@
+package com.imlog.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}

14c869133df2e2b43748717e5db106535439fc9b
newyoung124 <newyoung124@naver.com>
2023-04-23 23:50:42 +0900

    indexController 의 어노테이션 수정
    restcontroller -> controller
    getMapping 시 string 그대로 반환이 아닌
    .mustache 문서 찾아서 반환

--- src/main/java/com/imlog/web/IndexController.java
+++ src/main/java/com/imlog/web/IndexController.java
@@ -1,7 +1,11 @@
 package com.imlog.web;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
+@Controller
 public class IndexController {
 
     @GetMapping("/")

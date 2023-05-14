328518d9e90a8da74259fc4ee8ef6336e6cd838f
newyoung124 <newyoung124@naver.com>
2023-05-14 17:31:03 +0900

    로그인 기능

---
+++ src/main/java/com/imlog/config/auth/dto/SessionUser.java
@@ -0,0 +1,18 @@
+package com.imlog.config.auth.dto;
+
+import com.imlog.domain.user.User;
+
+import java.io.Serializable;
+
+public class SessionUser implements Serializable {
+
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}

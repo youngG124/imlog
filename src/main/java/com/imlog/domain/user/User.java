47e12a0aabe815e01fa29109522a4fe04a0dd6dc
newyoung124 <newyoung124@naver.com>
2023-06-21 16:02:24 +0900

    google login link

--- src/main/java/com/imlog/domain/user/User.java
+++ src/main/java/com/imlog/domain/user/User.java
@@ -5,7 +5,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @NoArgsConstructor

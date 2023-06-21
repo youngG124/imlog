79e8ee84ac2b2530ae5f10c2052b79f8dbb457f8
newyoung124 <newyoung124@naver.com>
2023-06-21 15:40:49 +0900

    6/21 restart google login

--- src/main/java/com/imlog/config/auth/dto/SessionUser.java
+++ src/main/java/com/imlog/config/auth/dto/SessionUser.java
@@ -1,9 +1,11 @@
 package com.imlog.config.auth.dto;
 
 import com.imlog.domain.user.User;
+import lombok.Getter;
 
 import java.io.Serializable;
 
+@Getter
 public class SessionUser implements Serializable {
 
     private String name;

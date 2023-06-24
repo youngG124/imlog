4f15f29a766d4dca9e26baad0a99c1a76beb44ff
newyoung124 <newyoung124@naver.com>
2023-06-24 20:43:39 +0900

    네이버 로그인

--- src/main/java/com/imlog/web/IndexController.java
+++ src/main/java/com/imlog/web/IndexController.java
@@ -1,30 +1,27 @@
 package com.imlog.web;
 
+import com.imlog.config.auth.LoginUser;
 import com.imlog.config.auth.dto.SessionUser;
 import com.imlog.service.posts.PostsService;
 import com.imlog.web.dto.PostsResponseDto;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+    public String index(Model model, @LoginUser SessionUser user) {
 
-        if (user != null) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user!=null) {
             model.addAttribute("userName", user.getName());
         }
 

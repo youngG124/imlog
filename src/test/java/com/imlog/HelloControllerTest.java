02d9590ae2c05fd8718675528852dc56b5875a4c
newyoung124 <newyoung124@naver.com>
2023-06-28 18:34:22 +0900

    기존 테스트에 시큐리티 적용하기 -> 스캔 대상에서 SecurityConfig 제거하고 EnableJpaAuditing 분리

--- src/test/java/com/imlog/HelloControllerTest.java
+++ src/test/java/com/imlog/HelloControllerTest.java
@@ -1,10 +1,14 @@
 package com.imlog;
 
+import com.imlog.config.auth.SecurityConfig;
 import com.imlog.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,12 +17,15 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+})
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello_will_be_returned() throws Exception {
         String hello = "hello";
@@ -26,6 +33,7 @@ public class HelloControllerTest {
         mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void 인사한다() throws Exception {
 
